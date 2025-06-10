0fa5081bbcc70c82785d0951d705a0a558529c06
Kaique Fernandes <kaique.1996@live.com>
2025-06-10 19:58:02 -0300

    Adicionando arquivos node_modules

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 15000,
+    pageLoadTimeout: 50000,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+  }
 });
